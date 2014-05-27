275480510a6474996887687edb27c774ea48b65c
Jarrod Eades <jarrod.eades@hotmail.com>
2014-05-27 10:50:49 +1000

    GUISimulator.java edited to be more capable. Have a basic UI in place with button that checks for negative CarPark values. Have the button check all the user inputted values to make sure they are in the correct format. Only one of these errors can occur at a time because java stops the continuation of the method.

--- src/asgn2Simulators/GUISimulator.java
+++ src/asgn2Simulators/GUISimulator.java
@@ -12,14 +12,19 @@ package asgn2Simulators;
 
 import java.awt.Color;
 import java.awt.Dimension;
+import java.awt.Font;
 import java.awt.GridLayout;
 import java.awt.HeadlessException;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 import javax.swing.BorderFactory;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTextArea;
 import javax.swing.JTextField;
 
 /**
@@ -27,8 +32,36 @@ import javax.swing.JTextField;
  *
  */
 @SuppressWarnings("serial")
-public class GUISimulator extends JFrame implements Runnable {
+public class GUISimulator extends JFrame implements Runnable, ActionListener {
 
+	// Initialise the text fields as global to use during events
+	JTextField maxCarSpacesTxt;
+	JTextField maxSmallCarSpacesTxt;
+	JTextField maxMotorCycleSpacesTxt;
+	JTextField maxQueueSizeTxt;
+	
+	private int maxCSpaces = Constants.DEFAULT_MAX_CAR_SPACES;
+	private int maxSCSpaces = Constants.DEFAULT_MAX_SMALL_CAR_SPACES;
+	private int maxMCSpaces = Constants.DEFAULT_MAX_MOTORCYCLE_SPACES;
+	private int maxQSize = Constants.DEFAULT_MAX_QUEUE_SIZE;
+	
+	JTextField seedTxt;
+	JTextField meanStayTxt;
+	JTextField sdStayTxt;
+	JTextField carProbTxt;
+	JTextField smallCarProbTxt;
+	JTextField mCProbTxt;
+	
+	private int seed = Constants.DEFAULT_SEED;
+	private double meanStay = Constants.DEFAULT_INTENDED_STAY_MEAN;
+	private double sdStay = Constants.DEFAULT_INTENDED_STAY_SD;
+	private double carProb = Constants.DEFAULT_CAR_PROB;
+	private double smallCarProb = Constants.DEFAULT_SMALL_CAR_PROB;
+	private double mCProb = Constants.DEFAULT_MOTORCYCLE_PROB;
+	
+	JTextArea errorArea;
+	private String newLine = "\n";
+	
 	/**
 	 * @param arg0
 	 * @throws HeadlessException
@@ -43,6 +76,7 @@ public class GUISimulator extends JFrame implements Runnable {
 	 */
 	@Override
 	public void run() {
+
 		final int WIDTH = 750;
 		final int HEIGHT = 500;
 	
@@ -58,19 +92,19 @@ public class GUISimulator extends JFrame implements Runnable {
 		JPanel carParkPanel = thePanelSettings();
 		
 		// Create a Header for the carPark settings
-		JLabel carParkLabel = new JLabel("Car Park Settings");
+		JLabel carParkLabel = theHeadingLabelSettings("Car Park Settings");
 		
 		// Create the labels for this panel
-		JLabel maxCarSpacesLbl = theLabelSettings("Max Car Spaces: ");
-		JLabel maxSmallCarSpacesLbl = theLabelSettings("Max Small Car Spaces: ");
-		JLabel maxMotorCycleSpacesLbl = theLabelSettings("Max MotorCycle Spaces: ");
-		JLabel maxQueueSizeLbl = theLabelSettings("Max Queue Size: ");
+		JLabel maxCarSpacesLbl = theLabelSettings("Max Car Spaces ");
+		JLabel maxSmallCarSpacesLbl = theLabelSettings("Max Small Car Spaces ");
+		JLabel maxMotorCycleSpacesLbl = theLabelSettings("Max MotorCycle Spaces ");
+		JLabel maxQueueSizeLbl = theLabelSettings("Max Queue Size ");
 		
 		// Create the text boxes
-		JTextField maxCarSpacesTxt = theTextFieldSettings();
-		JTextField maxSmallCarSpacesTxt = theTextFieldSettings();
-		JTextField maxMotorCycleSpacesTxt = theTextFieldSettings();
-		JTextField maxQueueSizeTxt = theTextFieldSettings();
+		maxCarSpacesTxt = theTextFieldSettings(""+maxCSpaces, maxCarSpacesLbl.getText());
+		maxSmallCarSpacesTxt = theTextFieldSettings(""+maxSCSpaces, maxSmallCarSpacesLbl.getText());
+		maxMotorCycleSpacesTxt = theTextFieldSettings(""+maxMCSpaces, maxMotorCycleSpacesLbl.getText());
+		maxQueueSizeTxt = theTextFieldSettings(""+maxQSize, maxQueueSizeLbl.getText());
 		
 		// Add the labels and text fields to the panel
 		carParkPanel.add(carParkLabel, "North");
@@ -93,23 +127,23 @@ public class GUISimulator extends JFrame implements Runnable {
 		JPanel simulatorPanel = thePanelSettings();
 		
 		// Create the Heading
-		JLabel simulatorLbl = theLabelSettings("Simulator Settings");
+		JLabel simulatorLbl = theHeadingLabelSettings("Simulator Settings");
 		
 		// Create the labels for this panel
-		JLabel seedLbl = theLabelSettings("Seed : ");
-		JLabel meanStayLbl = theLabelSettings("meanStay: ");
-		JLabel sdStayLbl = theLabelSettings("sdStay: ");
-		JLabel carProbLbl = theLabelSettings("Car Probability: ");
-		JLabel smallCarProbLbl = theLabelSettings("Small Car Probability: ");
-		JLabel mCProbLbl = theLabelSettings("MotorCycle Probability: ");
+		JLabel seedLbl = theLabelSettings("Seed ");
+		JLabel meanStayLbl = theLabelSettings("meanStay ");
+		JLabel sdStayLbl = theLabelSettings("sdStay ");
+		JLabel carProbLbl = theLabelSettings("Car Probability ");
+		JLabel smallCarProbLbl = theLabelSettings("Small Car Probability ");
+		JLabel mCProbLbl = theLabelSettings("MotorCycle Probability ");
 		
 		// Create the text boxes
-		JTextField seedTxt = theTextFieldSettings();
-		JTextField meanStayTxt = theTextFieldSettings();
-		JTextField sdStayTxt = theTextFieldSettings();
-		JTextField carProbTxt = theTextFieldSettings();
-		JTextField smallCarProbTxt = theTextFieldSettings();
-		JTextField mCProbTxt = theTextFieldSettings();
+		seedTxt = theTextFieldSettings(""+seed, seedLbl.getText());
+		meanStayTxt = theTextFieldSettings(""+meanStay, meanStayLbl.getText());
+		sdStayTxt = theTextFieldSettings(""+sdStay, sdStayLbl.getText());
+		carProbTxt = theTextFieldSettings(""+carProb, carProbLbl.getText());
+		smallCarProbTxt = theTextFieldSettings(""+smallCarProb, smallCarProbLbl.getText());
+		mCProbTxt = theTextFieldSettings(""+mCProb, mCProbLbl.getText());
 		
 		// Add the labels and text fields to the panel
 		simulatorPanel.add(simulatorLbl, "North");
@@ -139,13 +173,37 @@ public class GUISimulator extends JFrame implements Runnable {
 		JButton startButton = new JButton("Start the Simulation");
 		buttonPanel.add(startButton, "Center");
 		
+		startButton.addActionListener(this);
+		
+		// Create an Error box to show invalid inputs
+		JPanel errorPanel = new JPanel();
+		errorArea = new JTextArea(5, 20);
+		
+		// Set up a scroll in case of numerous errors
+		JScrollPane areaScrollPane = new JScrollPane(errorArea);
+		areaScrollPane.setVerticalScrollBarPolicy(
+		                JScrollPane.VERTICAL_SCROLLBAR_ALWAYS);
+		areaScrollPane.setHorizontalScrollBarPolicy(
+				JScrollPane.HORIZONTAL_SCROLLBAR_ALWAYS);
+		areaScrollPane.setPreferredSize(new Dimension(250, 250));
+		
+		// Set the error areas settings;
+		errorArea.setBackground(Color.LIGHT_GRAY);
+		errorArea.setEditable(false);
+		errorPanel.add(errorArea);
+		
+		// Create and add to the Settings Panel
 		JPanel settingsPanel = new JPanel();
 		settingsPanel.add(carParkPanel, "West");
 		settingsPanel.add(simulatorPanel, "East");
 		
+		// Create the run simulations panel
+		JPanel runSimPanel = new JPanel();
+		runSimPanel.add(errorPanel);
+		runSimPanel.add(buttonPanel);		
 		// Add the panel to the window
 		frame.add(settingsPanel, "North");
-		frame.add(buttonPanel, "South");
+		frame.add(runSimPanel, "South");
 		
 		//Display the window. 
 		frame.setPreferredSize(new Dimension(WIDTH, HEIGHT));
@@ -162,10 +220,47 @@ public class GUISimulator extends JFrame implements Runnable {
 		gui.run();
 	}
 
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
+		
+		// Reset GUI for new errors
+		reset();
+		
+		// Check Values are greater than zero
+		zeroGreaterThan(maxCarSpacesTxt);
+		zeroGreaterThan(maxMotorCycleSpacesTxt);
+		zeroGreaterThan(maxQueueSizeTxt);
+		zeroGreaterThan(maxSmallCarSpacesTxt);
+		
+		// Check that the maxCarSpaces is greater than the maxSmallCarSpaces
+		if(tryGettingInt(maxCarSpacesTxt) < tryGettingInt(maxSmallCarSpacesTxt)){
+			maxCarSpacesTxt.setBackground(Color.RED);
+			maxSmallCarSpacesTxt.setBackground(Color.RED);
+		}
+		
+		/*	Check that the values are all in the correct format
+		 *	Already checked maxCarSpaces, maxMotorCycleSpaces
+		 *	, maxQueueSize and maxSmallCarSpaces above
+		 */
+		tryGettingInt(seedTxt);
+		tryGettingDouble(meanStayTxt);
+		tryGettingDouble(sdStayTxt);
+		tryGettingDouble(carProbTxt);
+		tryGettingDouble(smallCarProbTxt);
+		tryGettingDouble(mCProbTxt);
+	}
+
+
 	private JPanel thePanelSettings(){
 		JPanel panel = new JPanel(new GridLayout(0,1));
 		panel.setBorder(BorderFactory.createLineBorder(Color.black));
 		return panel;
+	}
+	
+	private JLabel theHeadingLabelSettings(String words){
+		JLabel label = new JLabel(words);
+		return label;
 	}
 	
 	private JLabel theLabelSettings(String words){
@@ -173,9 +268,56 @@ public class GUISimulator extends JFrame implements Runnable {
 		return label;
 	}
 	
-	private JTextField theTextFieldSettings(){
-		JTextField textField = new JTextField();
+	private JTextField theTextFieldSettings(String value, String name){
+		JTextField textField = new JTextField(value);
+		textField.setName(name);
 		return textField;
 	}
+
+	@SuppressWarnings("null")
+	private int tryGettingInt(JTextField text){
+		try{
+			return Integer.parseInt(text.getText());
+		} catch (NumberFormatException e){
+			text.setBackground(Color.RED);
+			errorArea.setText(errorArea.getText() + text.getName() + " is not a integer (whole number)." + newLine);
+			return (Integer) null;
+		}	
+	}
 	
+	private Double tryGettingDouble(JTextField text){
+		try{
+			return Double.parseDouble(text.getText());
+		} catch (NumberFormatException e){
+			text.setBackground(Color.RED);
+			errorArea.setText(errorArea.getText() + text.getName() + " is not a number." + newLine);
+			return (Double) null;
+		}	
+	}
+	
+	private void zeroGreaterThan(JTextField text){
+		if(0 > tryGettingInt(text)){
+			text.setBackground(Color.RED);
+			errorArea.setText(errorArea.getText() + text.getName() + " is below zero." + newLine);
+		}
+	}
+
+	private void reset(){
+
+		// Clear error area of previous errors
+		errorArea.setText("");
+		
+		// Reset the background of the text fields;
+		maxCarSpacesTxt.setBackground(Color.WHITE);
+		maxSmallCarSpacesTxt.setBackground(Color.WHITE);
+		maxMotorCycleSpacesTxt.setBackground(Color.WHITE);
+		maxQueueSizeTxt.setBackground(Color.WHITE);
+		carProbTxt.setBackground(Color.WHITE);
+		smallCarProbTxt.setBackground(Color.WHITE);
+		mCProbTxt.setBackground(Color.WHITE);
+		seedTxt.setBackground(Color.WHITE);
+		meanStayTxt.setBackground(Color.WHITE);
+		sdStayTxt.setBackground(Color.WHITE);
+	}
+
 }
